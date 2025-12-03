59e8aafa06e2841d8565cbf443694df27ca19710
Artur Pelo <artur.pelo@gmail.com>
2025-12-03 18:26:17 +0100

    Add element structs and create an array of elements

--- proj_CPP/app.cpp
+++ proj_CPP/app.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <string>
 
+struct element{
+    float waga;
+    int cena; 
+};
+
+
 int main() {
 
+    element tab[10];
     std::string name;
 
-    std::cout<<"Podaj imię:";
+    std::cout<<" Podaj imię:";
     std::cin>>name;
     std::cout<<"Napisz program "<<name<<", który realizuje problem plecakowy! [polskie znaki bez problemu ĘŚĆŃął.....]";
 
